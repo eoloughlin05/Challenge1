ff4bfcfeb4b17c24997d777c80b21c7442bc4dbb
Erin McCauley <40056389@ads.qub.ac.uk>
2023-11-03 15:22:04 +0000

    initial commit

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    private float propellerSpeed = 1000;
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellerSpeed * Time.deltaTime);
+
+    }
+}
